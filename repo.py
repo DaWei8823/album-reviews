f4bf8f3886e80bc2a6c2a069face0206dff9457b
David Ullmann <davidl.ullmann@gmail.com>
2020-04-13 18:17:07 -0400

    renamed text_rank.py

--- repo.py
+++ repo.py
@@ -1,7 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
